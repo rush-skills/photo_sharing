43614dae98acf00ee32993b650bfe89bc796d3bd
Prelang Builder <builder@prelang.com>
2015-02-20 02:52:28 +0000

    Added #vote action to set model before_action in the votable model's controller

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,7 +3,7 @@ class PhotosController < ApplicationController
   #->Prelang (scaffolding:rails/scope_to_user)
   before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
 
-  before_action :set_photo, only: [:show, :edit, :update, :destroy]
+  before_action :set_photo, only: [:show, :edit, :update, :destroy, :vote]
 
   # GET /photos
   # GET /photos.json
