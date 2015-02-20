c757fdfa8f33ecb34e9f3cb47d42346f08233cf8
Prelang Builder <builder@prelang.com>
2015-02-20 02:52:42 +0000

    Added #vote action to set model before_action in the votable model's controller

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,7 +3,7 @@ class PortfoliosController < ApplicationController
   #->Prelang (scaffolding:rails/scope_to_user)
   before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
 
-  before_action :set_portfolio, only: [:show, :edit, :update, :destroy]
+  before_action :set_portfolio, only: [:show, :edit, :update, :destroy, :vote]
 
   # GET /portfolios
   # GET /portfolios.json
